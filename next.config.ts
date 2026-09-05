b1b60c096fa176a0d1d04182d6cd41fcb4dab4e0
CarloBarh <142626477+CarloBarh@users.noreply.github.com>
2026-09-04 22:32:54 -0600

    Remove an allowed development origin from config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    allowedDevOrigins: ["http://localhost:3000", "http://192.168.79.128:3000"],
+    allowedDevOrigins: ["http://localhost:3000"],
   },
 };
 
